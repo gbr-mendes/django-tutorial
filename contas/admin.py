084aa651bbbf4daf33044b620b90060249ad89b9
Gabriel <gabrielx165@gmail.com>
2022-05-28 12:29:06 -0300

    second commit

--- contas/admin.py
+++ contas/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+from .models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Persmissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important Dates'), {'fields': ('last_login',)}),
+        (_('Group Permissions'), {'fields': ('groups', 'user_permissions', )})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': 'wide',
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
--- core/models.py
+++ core/models.py
@@ -7,5 +7,16 @@ class Refeicao(models.Model):
     imagem = models.ImageField(upload_to="media/%Y/%m")
     descricao = models.TextField()
 
+    class TipoRefeicao(models.TextChoices):
+        ALMOCO = 'AL', ('Almoço')
+        JANTAR = 'JA', ('Jantar')
+
+
+    tipo_refeicao = models.CharField(
+        max_length=2,
+        choices=TipoRefeicao.choices,
+        default=TipoRefeicao.ALMOCO,
+    )
+
     def __str__(self):
         return self.nome
